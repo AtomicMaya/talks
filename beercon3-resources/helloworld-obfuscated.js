07a6d0a50b73e985d7c71de5cef6780037e794a5
AtomicNicos <nicolas.boeckh@etu.unige.ch>
2021-09-21 21:08:24 +0200

    progress

---
+++ beercon3-resources/helloworld-obfuscated.js
@@ -0,0 +1,48 @@
+function _0x620f() {
+	var _0x324169 = ['153274IEiVme', '126628dUMkzm', 'log', '171034Iwawbd', '6HpmGFI', '74388cEtUxk', '3458110hogRNf', '86202fbeHiR', '140cXhCfp', '40bOMbJc', '2223008SUdpXj'];
+	_0x620f = function() {
+		return _0x324169;
+	};
+	return _0x620f();
+}
+
+// a = 242, b
+function _0x2fd6(_0x48cc17, _0x28e01e) {
+	// Get array
+  var _0x620fea = _0x620f();
+  console.log("GET ARRAY", _0x620fea)
+
+	return _0x2fd6 = function(_0x2fd609, _0x4866b3) {
+    // a - 239 = 3
+		_0x2fd609 = _0x2fd609 - 0xef;
+		
+    var _0x135173 = _0x620fea[_0x2fd609];
+		return _0x135173;
+	}, _0x2fd6(_0x48cc17, _0x28e01e); // a = 242, b
+}
+
+var _0xe3e07f = _0x2fd6;
+
+(function(_0x5ac57d, _0x570c37) {
+	var _0x53aef6 = _0x2fd6,
+		_0x494d6c = _0x5ac57d();
+  
+    // true
+	while (!![]) {
+		try {
+                              /*  */
+			var _0x3266b2 = parseInt(_0x53aef6(0xf0)) / 0x1 + 
+        -parseInt(_0x53aef6(0xf3)) / 0x2 * (-parseInt(_0x53aef6(0xf4)) / 0x3) + 
+        -parseInt(_0x53aef6(0xf1)) / 0x4 * (-parseInt(_0x53aef6(0xf9)) / 0x5) + 
+        parseInt(_0x53aef6(0xf5)) / 0x6 * (parseInt(_0x53aef6(0xf8)) / 0x7) + 
+        -parseInt(_0x53aef6(0xef)) / 0x8 +
+        -parseInt(_0x53aef6(0xf7)) / 0x9 + 
+        -parseInt(_0x53aef6(0xf6)) / 0xa;
+			if (_0x3266b2 === _0x570c37) break;
+			else _0x494d6c['push'](_0x494d6c['shift']());
+		} catch (_0x4e1530) {
+			_0x494d6c['push'](_0x494d6c['shift']());
+		}
+	}
+}(_0x620f, 0x2ef03), console[_0xe3e07f(0xf2)]('Hello\x20World!'));
+// (array, 192259)                    (a = 0xf2 = 242)
